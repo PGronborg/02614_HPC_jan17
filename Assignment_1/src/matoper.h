2e2f53f9a27fe0c7b11c8f5c32a71a1437222d43
PGronborg <peter.gronborg.7@gmail.com>
2017-01-05 13:29:17 +0100

    Update matoper.h

--- Assignment_1/src/matoper.h
+++ Assignment_1/src/matoper.h
@@ -2,7 +2,8 @@
 void matadd_nat(int m,int k,double **A,double **B,double **C);
 void matsub_nat(int m,int k,double **A,double **B,double **C);
 double sum_mat(int m,int k,double **A);
-void matmul_nat(int m,int n,int k,double **A,double **B,double **C);
-void matmul_blk(int m,int n,int k,double **A,double **B,double **C,int blksize);
+void matmult_nat(int m,int n,int k,double **A,double **B,double **C);
+void matmult_blk(int m,int n,int k,double **A,double **B,double **C,int blksize);
 void printMat(int m, int n, double **A);
 void init_mat(int m, int k, double **A, double val);
+void matmult_lib(int m,int n,int k,double **A,double **B,double **C);
